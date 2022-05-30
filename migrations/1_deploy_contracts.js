96ad4696041907127ad96c0abe2bbd2deadd3a1e
SecondPort <gomezmanulm@gmail.com>
2022-05-30 15:00:59 -0300

    update

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,7 @@
+var Marketplace = artifacts.require("Marketplace");
 
-var SimpleStorage = artifacts.require("SimpleStorage");
-
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-};
+module.exports = async function(deployer) {
+  await deployer.deploy(Marketplace);
+  const marketplace = await Marketplace.deployed();
+  await deployer.deploy(BoredPetsNFT, marketplace.address);
+}
