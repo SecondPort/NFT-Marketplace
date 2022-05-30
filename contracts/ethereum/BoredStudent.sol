ad1199b6089bd41d903d1908f032bd66dc5a476c
SecondPort <gomezmanulm@gmail.com>
2022-05-30 13:33:26 -0300

    funcion mint

---
+++ contracts/ethereum/BoredStudent.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.4.20;
+
+import "../../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract BoredStudent is ERC721URIStorage{
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  address marketplaceContract;
+  event NFTMinted(uint256);
+
+  constructor(address _marketplaceContract) ERC721 ("Bored Student Club", "BSC"){
+    marketplaceContract = _marketplaceContract;
+  }
+
+  function mint(string memory _tokenURI)public {
+    _tokenIds.increment();
+    uint256 newTokenId = _tokenIds.current();
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, _tokenURI);
+    setApprovalForAll(marketplaceContract, true);
+    emit NFTMinted(newTokenId);
+  }
+}
