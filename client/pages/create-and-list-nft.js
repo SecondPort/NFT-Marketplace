147b23c528d9b16246c5cd093bd95bd13492178b
SecondPort <gomezmanulm@gmail.com>
2022-05-31 12:23:50 -0300

    subir los datos completos a ipfs

--- client/pages/create-and-list-nft.js
+++ client/pages/create-and-list-nft.js
@@ -1,42 +1,66 @@
 import {
     useState
-} from 'react'
-import Web3 from 'web3'
-import Web3Modal from 'web3modal'
+} from "react";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
 import {
     create as ipfsHttpClient
-} from 'ipfs-http-client'
+} from "ipfs-http-client";
 import {
     useRouter
-} from 'next/router'
+} from "next/router";
 
-const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
-import Marketplace from '../contracts/ethereum-contracts/Marketplace.json';
-import BoredStudent from '../contracts/ethereum-contracts/BoredStudent.json';
+import Marketplace from "../contracts/ethereum-contracts/Marketplace.json";
+import BoredStudent from "../contracts/ethereum-contracts/BoredStudent.json";
 
 export default function CreateItem() {
-    const [fileUrl, setFileUrl] = useState(null)
+    const [fileUrl, setFileUrl] = useState(null);
     const [formInput, updateFormInput] = useState({
-        price: '',
-        name: '',
-        description: ''
-    })
-    const router = useRouter()
+        price: "",
+        name: "",
+        description: "",
+    });
+    const router = useRouter();
 
     async function onChange(e) {
         // subir la imagen a ipfs
-        const file = e.target.files[0]
+        const file = e.target.files[0];
         try {
-            const added = await client.add(
-                file, {
-                    progress: (prog) => console.log(`received: ${prog}`)
-                }
-            )
-            const url = `https://ipfs.infura.io/ipfs/${added.path}`
-            setFileUrl(url)
+            const added = await client.add(file, {
+                progress: (prog) => console.log(`received: ${prog}`),
+            });
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+            setFileUrl(url);
         } catch (error) {
-            console.log('Error uploading file: ', error)
+            console.log("Error uploading file: ", error);
+        }
+    }
+
+    async function uploadToIPFS() {
+        const {
+            name,
+            description,
+            price
+        } = formInput;
+        if (!name || !description || !price || !fileUrl) {
+            return;
+        } else {
+            // primero se suben los metadatos a ipfs
+            const data = JSON.stringify({
+                name,
+                description,
+                image: fileUrl,
+            });
+            try {
+                const added = await client.add(data);
+                const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+                // después de cargar los metadatos en IPFS, devuelva la URL para usarla en la transacción
+                return url;
+            } catch (error) {
+                console.log("Error uploading file: ", error);
+            }
         }
     }
 }
