06e90fd2f5734425acd9e9cfd417ad58d14a6a88
SecondPort <gomezmanulm@gmail.com>
2022-05-31 12:18:37 -0300

    comprar nfts

--- client/pages/index.js
+++ client/pages/index.js
@@ -47,4 +47,45 @@ export default function Home(){
     setNfts(nfts.filter(nft => nft !== null))
     setLoadingState('loaded')
   }
+
+  async function buyNft(nft) {
+    const web3Modal = new Web3Modal()
+    const provider = await web3Modal.connect()
+    const web3 = new Web3(provider)
+    const networkId = await web3.eth.net.getId();
+    const marketPlaceContract = new web3.eth.Contract(Marketplace.abi, Marketplace.networks[networkId].address);
+    const accounts = await web3.eth.getAccounts();
+    await marketPlaceContract.methods.buyNft(BoredStudent.networks[networkId].address, nft.tokenId).send({ from: accounts[0], value: nft.price });
+    loadNFTs()
+  }
+  
+  if (loadingState === 'loaded' && !nfts.length) {
+    return (<h1 className="px-20 py-10 text-3xl">No pets available!</h1>)
+  } else {
+    return (
+      <div className="flex justify-center">
+        <div className="px-4" style={ { maxWidth: '1600px' } }>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+            {
+              nfts.map((nft, i) => (
+                <div key={i} className="border shadow rounded-xl overflow-hidden">
+                  <img src={nft.image} />
+                  <div className="p-4">
+                    <p style={ { height: '64px' } } className="text-2xl font-semibold">{nft.name}</p>
+                    <div style={ { height: '70px', overflow: 'hidden'  } }>
+                      <p className="text-gray-400">{nft.description}</p>
+                    </div>
+                  </div>
+                  <div className="p-4 bg-black">
+                    <p className="text-2xl font-bold text-white">{Web3.utils.fromWei(nft.price, "ether")} ETH</p>
+                    <button className="mt-4 w-full bg-teal-400 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
